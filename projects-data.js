542f9b10931541de551f10f3c1331441049b88c9
unknown <elrbyoda@gmail.com>
2025-12-02 12:07:57 +0200

    my p

---
+++ projects-data.js
@@ -0,0 +1,42 @@
+window.projects = [
+    {
+        id: 'arwear',
+        title: 'AR Wear',
+        description: 'Augmented reality clothing try-on application. Demonstrates AR integration and UX for virtual fitting.',
+        image: 'css/images/Ar_resized.png',
+        repo: 'https://github.com/11DON/ARwear.git',
+        demo: '',
+        tech: ['Flutter', 'ARCore'],
+        year: 2023
+    },
+    {
+        id: 'food-ordering',
+        title: 'Food ordering App',
+        description: 'Mobile app for ordering food with cart and checkout flows.',
+        image: 'css/images/phone2_bordered_1024x819.png',
+        repo: 'https://github.com/11DON/Food-Ordering-App.git',
+        demo: '',
+        tech: ['Flutter', 'Firebase'],
+        year: 2024
+    },
+    {
+        id: 'food-app',
+        title: 'Food App',
+        description: 'Recipe and food browsing application with images and categories.',
+        image: 'css/images/A2_processed_processed.jpg',
+        repo: 'https://github.com/11DON/foodapp',
+        demo: '',
+        tech: ['Android', 'SQLite'],
+        year: 2022
+    },
+    {
+        id: 'vital-link',
+        title: 'Vital Link',
+        description: 'Healthcare project focusing on connecting medical data with users.',
+        image: 'css/images/heart_image_matched.jpg',
+        repo: 'https://github.com/11DON/Vital-link',
+        demo: '',
+        tech: ['React', 'Node.js'],
+        year: 2024
+    }
+];
